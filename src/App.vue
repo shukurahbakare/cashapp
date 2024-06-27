34aa921c6a2b42b4d7890a7bc9861becfe8c9ebe
donice <doniceubaru1@gmail.com>
2024-06-27 13:40:37 +0100

    Fixed

--- src/App.vue
+++ src/App.vue
@@ -8,78 +8,50 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="You did it!" /> -->
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">For Businesses</RouterLink>
+        <RouterLink to="/partners">Payout Partners</RouterLink>
+        <RouterLink to="/about">About Us</RouterLink>
+        <RouterLink to="/careers">Careers</RouterLink>
+        <RouterLink to="/help">Help</RouterLink>
       </nav>
     </div>
+
+    <button>
+      <RouterLink to="/login">Download App</RouterLink>
+    </button>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use "./styles/index.scss" as *;
+
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 2px solid red;
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    display: flex;
+    gap: 1rem;
+    font-size: 1.2rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+    a.router-link-exact-active {
+      color: $primary;
+    }
+  }
+
+  button {
+    background-color: $primary;
+    border: 2px solid $primary;
   }
 }
 </style>
