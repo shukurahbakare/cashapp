1a84c11a6a10b2532dd3a0ebbf32ba61ada312a3
drdonice <doniceubaru1@gmail.com>
2024-06-28 11:50:32 +0100

    Fix

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <div class="container">
-      <div class="container">
-      <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
       <div class="wrapper">
         <!-- <HelloWorld msg="You did it!" /> -->
